50d26f3af0731d731dce479dcfe536e0ff3bd416
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2020-01-09 11:32:56 -0500

    Revert "update to react-scripts"
    This reverts commit 11ecbdab83713a99762dc35c88cb3afd19a6f42c.

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -40,7 +40,7 @@ const authToken = () => {
 
 const client = new ApolloClient({
   link: createUploadLink({
-    uri: 'https://profile-ms.da-an.ca',
+    uri: 'https://paas.beta.gccollab.ca/graphql',
     headers: {
       authorization: authToken(),
     },
