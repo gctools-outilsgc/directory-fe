42877e868f2547f432360658fad6569d6ab9a2f0
Phanoix <phanoix@gmail.com>
2020-05-25 13:38:30 -0400

    automatically create a few users and more complete client config

--- docker-e2e/account-init/acc-init.sh
+++ docker-e2e/account-init/acc-init.sh
@@ -9,6 +9,11 @@ python manage.py constance set SERVICE_MESH_HOST "rabbitMQ"
 python manage.py creatersakey
 
 python manage.py shell << EOF
+from core.models import User; 
+User.objects.create_superuser('admin', 'admin', 'adminpass')
+EOF
+
+python manage.py shell << EOF
 from oidc_provider.models import Client, ResponseType
 c = Client(name='profile', client_type='confidential', client_id='1', client_secret='123', redirect_uris=['http://profile_apollo:4000/'])
 c.save()
@@ -24,13 +29,13 @@ EOF
 
 python manage.py shell << EOF
 from oidc_provider.models import Client, ResponseType
-c = Client(name='directory', client_type='public', client_id='3', redirect_uris=['http://localhost:8008/'])
+c = Client(name='directory', client_type='public', client_id='3', website_url='http://localhost:8008', scope='openid modify_profile email profile', redirect_uris=['http://localhost:8008/#!callback', 'http://localhost:8008/#!silent'], post_logout_redirect_uris=['http://localhost:8008/#!logout'])
 c.save()
-c.response_types.add(ResponseType.objects.get(value='code'))
+c.response_types.add(ResponseType.objects.get(value='id_token token'))
 EOF
 
 
-
+apk add curl
 curl -XPUT -H "Content-type: application/json" -d '{
   "mappings": {
     "properties": {
@@ -143,3 +148,24 @@ curl -XPUT -H "Content-type: application/json" -d '{
     }
   }
 }' 'http://es:9200/profiles'
+
+
+python manage.py shell << EOF
+from core.models import User;
+from django.core import signing
+user = User.objects.create_user('user@gccollab.ca', 'user', 'pass')
+user.activate_user(signing.dumps(obj=user.email))
+EOF
+sleep 6
+python manage.py shell << EOF
+from core.models import User;
+from django.core import signing
+user = User.objects.create_user('user2@gccollab.ca', 'user2', 'pass')
+user.activate_user(signing.dumps(obj=user.email))
+EOF
+python manage.py shell << EOF
+from core.models import User;
+from django.core import signing
+user = User.objects.create_user('user3@gccollab.ca', 'user3', 'pass')
+user.activate_user(signing.dumps(obj=user.email))
+EOF
