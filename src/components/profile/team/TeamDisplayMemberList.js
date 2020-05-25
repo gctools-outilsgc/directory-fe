8f104292558b6ff5c624bbd29b9e5672892405d7
Nick <n.pietrantonio0024@live.com>
2020-05-25 11:23:36 -0400

    Change to team query and render (#99)
    * change how team members get queried and rendered + do not display global team members
    
    * adding translations

--- src/components/profile/team/TeamDisplayMemberList.js
+++ src/components/profile/team/TeamDisplayMemberList.js
@@ -1,47 +1,46 @@
+/* eslint-disable */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Col, Row } from 'reactstrap';
+import { Query } from 'react-apollo';
 
+import { GET_TEAM_MEMBERS } from '../../../gql/profile';
 import { UserAvatar } from '../../core/UserAvatar';
+import Loading from './Loading';
 
-const TeamDisplayMemberList = (props) => {
-  const { members } = props;
-  const list = (members) ?
-    members.map(p => (
-      <Col sm="4" className="mb-3" key={p.gcID}>
-        <a href={p ? p.gcID : ''} >
-          <div className="d-flex">
-            <UserAvatar
-              avatar={p ? p.avatar : ''}
-              name=""
-            />
-            <div className="ml-2 font-weight-bold">
-              <div className="text-dark">
-                {p ? p.name : 'None'}
-              </div>
-              {(localizer.lang === 'en_CA') ? (
-                <small className="text-muted">
-                  {p ? p.titleEn : ''}
-                </small>
-              ) : (
-                <small className="text-muted">
-                  {p ? p.titleFr : ''}
-                </small>
-              )}
-            </div>
-          </div>
-        </a>
-      </Col>
-    )) : 'No Team';
+const MemberList = (props) => {
+  const { p } = props;
   return (
-    <Row className="mt-3">
-      {list}
-    </Row>
+    <Col sm="4" className="mb-3" key={p.gcID}>
+      <a href={p ? p.gcID : ''} >
+        <div className="d-flex">
+          <UserAvatar
+            avatar={p ? p.avatar : ''}
+            name=""
+          />
+          <div className="ml-2 font-weight-bold">
+            <div className="text-dark">
+              {p ? p.name : 'None'}
+            </div>
+            {(localizer.lang === 'en_CA') ? (
+              <small className="text-muted">
+                {p ? p.titleEn : ''}
+              </small>
+            ) : (
+              <small className="text-muted">
+                {p ? p.titleFr : ''}
+              </small>
+            )}
+          </div>
+        </div>
+      </a>
+    </Col>
   );
 };
-
+/*
 TeamDisplayMemberList.propTypes = {
+  userID: PropTypes.isRequired,
   members: PropTypes.arrayOf(PropTypes.shape({
     gcID: PropTypes.string,
     avatar: PropTypes.string,
@@ -50,5 +49,34 @@ TeamDisplayMemberList.propTypes = {
     titleFr: PropTypes.string,
   })).isRequired,
 };
+*/
+const TeamDisplayMemberList = (props) => {
+  const { userID } = props;
+  return (
+    <Query
+      query={GET_TEAM_MEMBERS}
+      variables={{ gcID: (String(userID)) }}
+    >
+      {({
+        loading,
+        error,
+        data,
+      }) => {
+        if (loading) return <Loading />;
+        if (error) return errorHandler(error);
+        const members = (!data) ? '' : data.profiles[0].team.members;
+        const list = (members) ?
+          members.map(p => (
+            <MemberList p={p} />
+          )): 'N/A';
+        return(
+          <Row className="mt-3">
+            {list}
+          </Row>
+        );
+      }}
+    </Query>
+  );
+}
 
 export default TeamDisplayMemberList;
