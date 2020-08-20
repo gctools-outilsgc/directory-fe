1d2a7da6c23f3f9ddaf7fdd3686d64872c05b465
Ilia <phanoix@gmail.com>
2020-08-20 13:36:54 -0400

    Update brigade.js (#160)

--- brigade.js
+++ brigade.js
@@ -18,7 +18,7 @@ events.on("image_push", function(e, project) {
   notify.env.CHATKEY = project.secrets.chatKey
   notify.tasks = [
     "apk update && apk add curl",
-    "curl -X POST -H 'Content-Type: application/json' --data '{\"username\":\"Brigade\",\"icon_emoji\":\":k8s:\",\"text\":\"Directory prod deployment updated.\"}' https://message.gccollab.ca/hooks/$CHATKEY"      //rocket chat notification webhook
+    "curl -X POST -H 'Content-Type: application/json' --data '{\"username\":\"Brigade\",\"icon_emoji\":\":k8s:\",\"text\":\"Directory prod deployment updated.\",\"attachments\":[{\"title\":\"https://profile.gccollab.ca\",\"title_link\": \"https://profile.gccollab.ca\",\"text\": \"Rolling update in progress.\",\"color\":\"#764FA5\"}]}' https://message.gccollab.ca/hooks/$CHATKEY"      //rocket chat notification webhook
   ]
   
   update.run().then(() => { notify.run() })
